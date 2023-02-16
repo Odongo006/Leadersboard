05f57fc474e2fc8b5318b3a4e027dc40b855b14f
Odongo006 <www.henryodongo006@gmail.com>
2023-02-16 20:44:36 +0300

    change webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
   entry: {
     index: './src/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 8080,
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -18,18 +27,13 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.html$/i,
-        loader: 'html-loader',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ],
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
   },
 };
